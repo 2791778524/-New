e6a285ebbc63166f1b87b4524c8b65162e47220d
lizilong <2791778524@qq.com>
2022-04-26 11:20:19 +0800

    提交代码

--- src/components/List.vue
+++ src/components/List.vue
@@ -69,6 +69,8 @@ export default {
       //向上取整start的位置
       this.start = Math.floor(scrollTop / this.size)
       this.end = this.start + this.showNumber;
+      console.log(this.start, this.end);
+      console.log(this.end, this.start);
     },
   },
 };
